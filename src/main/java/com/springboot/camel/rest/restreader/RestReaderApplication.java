631d25bdd9980aae9ac962fe6751b3f44d798392
CPPIB\gzhou <gzhou@cppib.com>
2018-01-04 11:06:00 -0500

    1st work version

--- src/main/java/com/springboot/camel/rest/restreader/RestReaderApplication.java
+++ src/main/java/com/springboot/camel/rest/restreader/RestReaderApplication.java
@@ -1,13 +1,23 @@
 package com.springboot.camel.rest.restreader;
 
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestReaderApplication {
+public class RestReaderApplication implements CommandLineRunner {
+
+	@Produce(uri="direct:start")
+	ProducerTemplate startEndpt;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(RestReaderApplication.class, args);
+	}
+
+	public void run(String... args) {
+		startEndpt.sendBody(null);
 	}
 }
--- src/main/java/com/springboot/camel/rest/restreader/RestReaderRouteBuilder.java
+++ src/main/java/com/springboot/camel/rest/restreader/RestReaderRouteBuilder.java
@@ -8,10 +8,10 @@ public class RestReaderRouteBuilder extends RouteBuilder{
 
     @Override
     public void configure() throws Exception {
-        //from("direct:start").
-        from("cxfrs:http://localhost:8086/rest/clubmembers")
-        //to("http://localhost:8086/rest/clubmembers")
-                .process(new MyProcessor());
-             //   .to("bean:persistenceBean?method=persistIncomingData");
+        from("direct:start").
+                //to("cxfrs:http://localhost:8086/rest/clubmembers")
+        to("http://localhost:8086/rest/clubmembers").
+             //   .process(new MyProcessor());
+        to("bean:persistenceBean?method=persistIncomingData");
     }
 }
